b6d7f4bc97390dc602ad05fd1959b0e49c75c6a4
Thiago <thiagocarv4@gmail.com>
2026-04-30 20:41:09 -0300

    feat: expandindo o escopo do spring

--- src/main/java/br/com/thiago/primeiro_proj/PrimeiroProjApplication.java
+++ src/main/java/br/com/thiago/primeiro_proj/PrimeiroProjApplication.java
@@ -2,8 +2,10 @@ package br.com.thiago.primeiro_proj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("br.com.thiago")
 public class PrimeiroProjApplication {
 
 	public static void main(String[] args) {
