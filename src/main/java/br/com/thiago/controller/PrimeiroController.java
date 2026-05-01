540894b1b47168496c40e7663a31d7627bac5fa6
Thiago <thiagocarv4@gmail.com>
2026-04-30 21:13:25 -0300

    feat: lidando com parametros de rota

--- src/main/java/br/com/thiago/controller/PrimeiroController.java
+++ src/main/java/br/com/thiago/controller/PrimeiroController.java
@@ -1,15 +1,29 @@
 package br.com.thiago.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/primeiroController")
 public class PrimeiroController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "Parabéns! Você acessou sua primeira rota com Spring Boot!";
+    @GetMapping("/{id}")
+    public String recuperarId(@PathVariable Long id) {
+        return "ID recebido: " + id;
+    }
+
+    @GetMapping("/buscar")
+    public String recuperarQueryParam(@RequestParam String nome) {
+        return "Query param recebido - nome: " + nome;
+    }
+
+    @GetMapping("/filtros")
+    public String recuperarTodosQueryParams(@RequestParam Map<String, String> params) {
+        return "Query params recebidos: " + params;
     }
 }
